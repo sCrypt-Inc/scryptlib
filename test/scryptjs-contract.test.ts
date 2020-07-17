22e6abd50e51a218c65d38e95f3eeb84df48dbe5
Xiaohui <whulxh@gmail.com>
2020-07-17 12:23:08 -0700

    refactor contract test

--- test/scryptjs-contract.test.ts
+++ test/scryptjs-contract.test.ts
@@ -10,6 +10,7 @@ const sig = '30440220729d3935d496e5a708a6a1d4c61dcdd1bebae6f0e0b63b9b9eb1b7616cd
 const pubKey = '03613fa845ad3fe1ef4fe9bbf0b50a1cb5219dd30a0c4e3e4e46fb218313af9220';
 const txHex = '01000000015884e5db9de218238671572340b207ee85b628074e7e467096c267266baf77a40000000000ffffffff0000000000';
 const tx = deserialize(txHex);
+const inputSatoshis = 100000;
 
 describe('getContractClass()', () => {
 
@@ -49,32 +50,31 @@ describe('getContractClass()', () => {
     describe('verify()', () => {
 
       const unlockingScript = '30440220729d3935d496e5a708a6a1d4c61dcdd1bebae6f0e0b63b9b9eb1b7616cdbbc2b02203b58cdde0133a6e90d921ecee6ecafca7000a13a3e38673810b4c6badd8d952041 03613fa845ad3fe1ef4fe9bbf0b50a1cb5219dd30a0c4e3e4e46fb218313af9220';
-      const inputSatoshis = 100000;
       const txContext = { hex: txHex };
 
-      describe('when instance.txContext was unset', () => {
-        it('should return true if all params are appropriate', () => {
+      describe('when instance.txContext is unset', () => {
+        it('should return true if all arguments are correct', () => {
           assert.isTrue(instance.verify(unlockingScript, inputSatoshis, txContext));
         })
       })
 
-      describe('when instance.txContext was set', () => {
-        it('should return true if other params (except txContext) is appropriate', () => {
+      describe('when instance.txContext is set', () => {
+        it('should return true if other arguments (except txContext) is correct', () => {
           instance.txContext = txContext;
           assert.isTrue(instance.verify(unlockingScript, inputSatoshis));
         })
       })
 
-      it('should return false if param `unlockingScript` is inappropriate', () => {
+      it('should return false if param `unlockingScript` is wrong', () => {
         assert.isFalse(instance.verify(unlockingScript + '00', inputSatoshis, txContext));
       })
 
-      it('should return false if param `inputSatoshis` is inappropriate', () => {
+      it('should return false if param `inputSatoshis` is wrong', () => {
         assert.isFalse(instance.verify(unlockingScript, inputSatoshis + 1, txContext));
       })
 
-      it('should return false if param `txContext` is inappropriate', () => {
-        assert.isFalse(instance.verify(unlockingScript, inputSatoshis + 1, undefined));
+      it('should return false if param `txContext` is wrong', () => {
+        assert.isFalse(instance.verify(unlockingScript, inputSatoshis, undefined));
       })
     })
 
@@ -84,11 +84,12 @@ describe('getContractClass()', () => {
 
     describe("when the mapped-method being invoked", () => {
       it("should return ScriptedMethodCall type object which could be transformed to locking script", () => {
-        assert.instanceOf(instance.unlock(sig, pubKey), ScriptedMethodCall);
-        assert.equal(instance.unlock(sig, pubKey).toASM(), '30440220729d3935d496e5a708a6a1d4c61dcdd1bebae6f0e0b63b9b9eb1b7616cdbbc2b02203b58cdde0133a6e90d921ecee6ecafca7000a13a3e38673810b4c6badd8d952041 03613fa845ad3fe1ef4fe9bbf0b50a1cb5219dd30a0c4e3e4e46fb218313af9220');
-        assert.equal(instance.unlock(sig, pubKey).toHex(), '4730440220729d3935d496e5a708a6a1d4c61dcdd1bebae6f0e0b63b9b9eb1b7616cdbbc2b02203b58cdde0133a6e90d921ecee6ecafca7000a13a3e38673810b4c6badd8d9520412103613fa845ad3fe1ef4fe9bbf0b50a1cb5219dd30a0c4e3e4e46fb218313af9220');
-        assert.isTrue(instance.unlock(sig, pubKey).verify(100000, { hex: txHex }))
-        assert.isTrue(instance.unlock(sig, pubKey).verify(100000, { tx }))
+        const methodCall = instance.unlock(sig, pubKey);
+        assert.instanceOf(methodCall, ScriptedMethodCall);
+        assert.equal(methodCall.toASM(), '30440220729d3935d496e5a708a6a1d4c61dcdd1bebae6f0e0b63b9b9eb1b7616cdbbc2b02203b58cdde0133a6e90d921ecee6ecafca7000a13a3e38673810b4c6badd8d952041 03613fa845ad3fe1ef4fe9bbf0b50a1cb5219dd30a0c4e3e4e46fb218313af9220');
+        assert.equal(methodCall.toHex(), '4730440220729d3935d496e5a708a6a1d4c61dcdd1bebae6f0e0b63b9b9eb1b7616cdbbc2b02203b58cdde0133a6e90d921ecee6ecafca7000a13a3e38673810b4c6badd8d9520412103613fa845ad3fe1ef4fe9bbf0b50a1cb5219dd30a0c4e3e4e46fb218313af9220');
+        assert.isTrue(methodCall.verify(inputSatoshis, { hex: txHex }))
+        assert.isTrue(methodCall.verify(inputSatoshis, { tx }))
       })
     })
 
