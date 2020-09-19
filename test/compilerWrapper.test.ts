2323098aaa30dfd77fbf1e6b9a9a9b516aefb413
cuilei <leoncui57@gmail.com>
2020-09-18 18:05:42 -0700

    update

--- test/compilerWrapper.test.ts
+++ test/compilerWrapper.test.ts
@@ -1,31 +1,31 @@
 import { assert } from 'chai';
-import { compile,CompileResult } from '../src/compilerWrapper';
-import path = require("path")
+import { compile, CompileResult } from '../src/compilerWrapper';
+import path = require("path");
 
 
 
-describe('compile()', () => {     
-    it('compile successfully', () => {    
-      const result = compileContract("demo.scrypt","fixture");
+describe('compile()', () => {
+  it('compile successfully', () => {
+    const result = compileContract("p2pkh.scrypt", "fixture");
 
-      assert.typeOf(result, 'object');
-      assert.equal(result.errors.length,0,"No Errors")
-    })
+    assert.typeOf(result, 'object');
+    assert.equal(result.errors.length, 0, "No Errors");
+  })
 
 
-    it('should return SyntaxError', () => {
-      const result = compileContract("demo_wrong.scrypt","fixture/invalid");
+  it('should return SyntaxError', () => {
+    const result = compileContract("p2pkh_wrong.scrypt", "fixture/invalid");
 
-      assert.notEqual(result.errors.length,0,"No Errors")
-      assert.include(result.errors[0].type, 'SyntaxError', 'contract has SyntaxError');
-    })
+    assert.isAbove(result.errors.length, 0, "exist Errors");
+    assert.include(result.errors[0].type, 'SyntaxError', 'contract has SyntaxError');
+  })
 })
 
-function compileContract(fileName:string, folder:string){
+function compileContract(fileName: string, folder: string): CompileResult {
   const filePath = path.join(__dirname, folder, fileName);
   const result = compile(
     { path: filePath },
     { desc: true, outputDir: path.join(__dirname, '') }
-  )
-  return result
+  );
+  return result;
 }
