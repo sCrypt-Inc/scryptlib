05d97529d6cd9a4ac706b421b43a76c6e8c88d62
hh <zhfnjust@qq.com>
2021-02-26 16:49:39 +0800

    add subArrayType

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export { Arguments , Argument } from './abi';
 export {
     bsv, signTx, toHex, getPreimage, num2bin, bin2num, bool2Asm, int2Asm, parseLiteral, bytes2Literal, bytesToHexString, getValidatedHexString, literal2ScryptType, literal2Asm,
     findStructByType, findStructByName, checkStruct, isStructType, isArrayType, compileContract,
-    arrayTypeAndSize, newCall, getStructNameByType, genLaunchConfigFile
+    arrayTypeAndSize, newCall, getStructNameByType, genLaunchConfigFile, subArrayType
 } from './utils';
 export { serializeState, deserializeState, State, STATE_LEN_2BYTES, STATE_LEN_4BYTES } from './serializer';
 export { Int, Bool, Bytes, PrivKey, PubKey, Sig, Ripemd160, Sha1, Sha256, SigHashType, SigHashPreimage, OpCodeType, SingletonParamType, SupportedParamType, ScryptType, ValueType, 
