074edaa157a762b32e1b3fbfeeb4d5a5b6f0e07e
Philip Song <philjunsong@gmail.com>
2021-10-20 23:15:41 -0700

    npx using compileWrapper

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,7 @@
 const os = require('os');
 const path = require('path');
 const { exec } = require("child_process");
+const { compile } = require('../dist/compilerWrapper.js');
 
 function main() {
   let FILENAME = "Windows.exe";
@@ -12,17 +13,18 @@ function main() {
   }
 
   let scryptBinaryPath = process.env.npm_config_user_agent ? `./node_modules/scryptlib/bin/scryptc-${FILENAME}` : `./bin/scryptc-${FILENAME}`;
-  exec(`${scryptBinaryPath} ${process.argv.slice(2).join(' ')}`, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+  compile(
+    {
+      path: process.argv.slice(2)[0]  //  the file path of the contract
+    },
+    {
+      desc: true,
+      asm: true,
+      optimize: false,
+      sourceMap: true,
+      cmdPrefix: scryptBinaryPath
     }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`${stdout}`);
-  });
+  );
 }
 
 if (require.main === module) {
