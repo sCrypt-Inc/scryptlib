89fc50b126e56fa5bad37ace3546027b3e49e05c
hh <hh@scrypt.io>
2022-02-17 11:12:25 +0800

    add export function

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,18 @@ export {
   readLaunchJson, getLowSPreimage, readBytes, parseAbiFromUnlockingScript, findConstStatic, findStatic, resolveConstValue,
   arrayTypeAndSizeStr, isLibraryType, toLiteralArrayType, serializeSupportedParamType, shortType, getLibraryNameByType,
   librarySign, structSign, resolveGenericType, createDefaultLibrary, createLibraryProperties, canAssignProperty, arrayToLiteral, cloneArray, arrayToScryptType,
-  buildStructsClass, buildTypeResolver, buildScryptTypeResolver, getStructDeclaration, getABIDeclaration, typeOfArg, buildContractState
+  buildStructsClass, buildTypeResolver, buildScryptTypeResolver, getStructDeclaration, getABIDeclaration, typeOfArg, buildContractState,
+  compilerVersion, flatternLibrary, flatternLibraryState, flatternCtorArgs, flatternStruct, flatternArray, flatternParams, flatternStateArgs,
+  isEmpty
 } from './internal';
 
 export {
-  Int, Bool, Bytes, PrivKey, PubKey, Sig, Ripemd160, Sha1, Sha256, SigHashType, SigHashPreimage,
+  Int, Bool, Bytes, PrivKey, PubKey, Sig, Ripemd160, Sha1, Sha256, SigHashType, SigHashPreimage, String,
   OpCodeType, SingletonParamType, SupportedParamType, ScryptType, ValueType, PubKeyHash, TxContext,
   Struct, StructObject, VariableType, TypeResolver, PrimitiveTypes, Library, ScryptTypeResolver, AsmVarValues,
-  RawTypes, SigHash, Arguments, Argument, State, StructEntity, LibraryEntity, ABIEntity, ABIEntityType, ABI, ParamEntity, BuildType, RelatedInformation, ContractDescription, VerifyResult, VerifyError, AbstractContract
+  RawTypes, SigHash, Arguments, Argument, State, StructEntity, LibraryEntity, ABIEntity, ABIEntityType, ABI, ParamEntity,
+  BuildType, RelatedInformation, ContractDescription, VerifyResult, VerifyError, AbstractContract,
+  DebugInfo, DebugModeTag
 } from './internal';
 
 //Equivalent to the built-in functions
