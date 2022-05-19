f926740c2b93757e06cac523a52198c84512dcba
hh <hh@scrypt.io>
2022-05-19 11:22:25 +0800

    move getRandomInt to helper.ts

--- test/helper.ts
+++ test/helper.ts
@@ -47,3 +47,9 @@ export function excludeMembers(o: any, members: string[]) {
 
   return o;
 }
+
+export function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
