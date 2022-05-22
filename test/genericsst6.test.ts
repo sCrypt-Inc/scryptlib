e3eb2b3ce7b6020ce9ecfa33778a1e2408e49d5e
hh <hh@scrypt.io>
2022-05-22 22:24:08 +0800

    add test

--- test/genericsst6.test.ts
+++ test/genericsst6.test.ts
@@ -2,33 +2,39 @@
 import { assert, expect } from 'chai';
 import { newTx, loadDescription, getContractFilePath } from './helper';
 import { buildContractClass, VerifyError, buildTypeClasses } from '../src/contract';
-import { Bytes } from '../src';
-import { HashedMap } from '../src/scryptTypes';
-
 
 describe('GenericStruct  test', () => {
 
-    describe('test genericsst5', () => {
+    describe('test genericsst6', () => {
         let c, result;
 
-        const C = buildContractClass(loadDescription('genericsst5_desc.json'));
-        const { ST0, ST1, ERC20} = buildTypeClasses(C);
+        const C = buildContractClass(loadDescription('genericsst6_desc.json'));
+        const { ST0, ST1, ST2,L} = buildTypeClasses(C);
         before(() => {
-            const erc20 = new ERC20(
-                1000000,
-                new HashedMap(new Bytes(''))
-            );
-            c = new C(erc20);
+
+            c = new C(new L(new ST2({
+                x: new ST1({
+                    x: new ST0({
+                        x: 1,
+                        y: [2,3]
+                    }),
+                    y: [4,5]
+                }),
+                y: 100
+            })));
         });
 
         it('should unlock successfully', () => {
 
-            result = c.unlock(new ST1({
-                x: new ST0({
-                    x: false,
-                    y: 3000
+            result = c.unlock(new ST2({
+                x: new ST1({
+                    x: new ST0({
+                        x: 1,
+                        y: [2,3]
+                    }),
+                    y: [4,5]
                 }),
-                y: 2000
+                y: 100
             })).verify();
 
             expect(result.success, result.error).to.be.true
@@ -36,12 +42,15 @@ describe('GenericStruct  test', () => {
 
 
         it('should unlock fail', () => {
-            result = c.unlock(new ST1({
-                x: new ST0({
-                    x: false,
-                    y: 3000
+            result = c.unlock(new ST2({
+                x: new ST1({
+                    x: new ST0({
+                        x: 1,
+                        y: [2,3]
+                    }),
+                    y: [4,5]
                 }),
-                y: 2001
+                y: 101
             })).verify();
 
             expect(result.success, result.error).to.be.false
